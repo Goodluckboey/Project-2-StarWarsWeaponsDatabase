8fd17fe2d67830362439065689ddac7030932541
PatrickB <84555143+Goodluckboey@users.noreply.github.com>
2021-12-16 16:33:08 +0800

    starting on bounties

--- src/pages/Bounties.js
+++ src/pages/Bounties.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
 import { NavLink } from "react-router-dom";
